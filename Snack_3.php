233f5759018b26183ffcc6036718372e3570fdfc
G-Salis <giuseppesalislavoro@gmail.com>
2021-12-20 17:20:22 +0100

    Snack 3, 4, 5 ,6

---
+++ Snack_3.php
@@ -0,0 +1,108 @@
+<?php
+
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+print_r(array_keys($posts[0]));
+
+var_dump($posts);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Snack 3</title>
+</head>
+<body>
+<div>
+<?php
+  for($i=0; $i< count($posts['10/01/2019']); $i++){
+    echo (array_keys($posts));
+    echo ". ";
+    echo $posts["10/01/2019"][$i]["title"];
+    echo ": ";
+    echo $posts["10/01/2019"][$i]["author"];
+    echo ". ";
+    echo " - ";
+    echo $posts["10/01/2019"][$i]["text"];
+    echo "<br>";
+   
+  }
+  ?>
+</div>
+<div>
+<?php
+  for($i=0; $i< count($posts['10/02/2019']); $i++){
+    echo (array_keys($posts));
+    echo ". ";
+    echo $posts["10/02/2019"][$i]["title"];
+    echo ": ";
+    echo $posts["10/02/2019"][$i]["author"];
+    echo ". ";
+    echo " - ";
+    echo $posts["10/02/2019"][$i]["text"];
+    echo "<br>";
+   
+  }
+  ?>
+</div>
+<div>
+<?php
+  for($i=0; $i< count($posts['15/05/2019']); $i++){
+    echo (array_keys($posts));
+    echo ". ";
+    echo $posts["15/05/2019"][$i]["title"];
+    echo ": ";
+    echo $posts["15/05/2019"][$i]["author"];
+    echo ". ";
+    echo " - ";
+    echo $posts["15/05/2019"][$i]["text"];
+    echo "<br>";
+   
+  }
+  ?>
+</div>
+
+
+  
+</body>
+</html>
---
+++ Snack_5.php
@@ -0,0 +1,28 @@
+<?php
+
+
+$parag ="Gregorio Samsa, svegliandosi una mattina da sogni agitati, si trovò trasformato, nel suo letto, in un enorme insetto immondo. Riposava sulla schiena, dura come una corazza, e sollevando un poco il capo vedeva il suo ventre arcuato, bruno e diviso in tanti segmenti ricurvi, in cima a cui la coperta da letto, vicina a scivolar giù tutta, si manteneva a fatica. Le gambe, numerose e sottili da far pietà, rispetto alla sua corporatura normale, tremolavano senza tregua in un confuso luccichio dinanzi ai suoi occhi. Cosa m’è avvenuto? pensò. Non era un sogno. La sua camera, una stanzetta di giuste proporzioni, soltanto un po’ piccola, se ne stava tranquilla fra le quattro ben note pareti. Sulla tavola, un campionario disfatto di tessuti - Samsa era commesso viaggiatore e sopra, appeso alla parete, un ritratto, ritagliato da lui - non era molto - da una rivista illustrata e messo dentro una bella cornice dorata: raffigurava una donna seduta, ma ben dritta sul busto, con un berretto e un boa di pelliccia; essa levava incontro a chi guardava un pesante manicotto, in cui scompariva tutto l’avambraccio";
+
+  $new_parag = explode('.', $parag);
+
+  var_dump($new_parag);
+
+  for ($i = 0; $i < count($new_parag); $i++){
+    echo $new_parag[$i] . ".<br>";
+  }
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Snack 5</title>
+</head>
+<body>
+  
+</body>
+</html>
